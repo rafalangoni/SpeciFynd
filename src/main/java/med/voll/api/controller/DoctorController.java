02b39043c59e4ac4b78ba7949d3cf08de6806e1e
Rafael Langoni <langoninaves@gmail.com>
2024-09-23 06:24:47 -0230

    Creating get method to return a list of doctors with 4 attributes using DTO; implementing pagination and showing sql statements

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,15 +1,18 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.doctor.DataListingDoctor;
 import med.voll.api.doctor.Doctor;
 import med.voll.api.doctor.DoctorRegistrationData;
 import med.voll.api.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctors")
@@ -23,4 +26,28 @@ public class DoctorController {
     public void registration(@RequestBody @Valid DoctorRegistrationData data) {
         repository.save(new Doctor(data));
     }
+
+    @GetMapping
+    public Page<DataListingDoctor> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
+        return repository.findAll(pagination).map(DataListingDoctor::new);
+
+    }
+
+
+//    @GetMapping
+//    public List<DataListingDoctor> list(Pageable pagination){
+//        return repository.findAll(pagination)
+//                .stream()
+//                .map(doctor -> new DataListingDoctor(doctor))
+//                .toList();
+//        //This map above can be written two different ways like this:
+////                .map(DataListingDoctor::new)
+//    }
+    // If I dont want to use Page and Pageable, I can return a list instead os pagination
+//                .stream()
+//                .map(doctor -> new DataListingDoctor(doctor))
+//                .toList();
+//                This map above can be written two different ways like this:
+//                .map(DataListingDoctor::new)
+
 }
