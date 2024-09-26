d265d730eff1c9bc5f01efa6891ad9cbfd2c9abf
Rafael Langoni <langoninaves@gmail.com>
2024-09-26 05:26:55 -0230

    put for patient, listing for patient and add on controller; besides a new migration to create patients table on MySql to store the data

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,13 +1,10 @@
 package med.voll.api.controller;
 
-import med.voll.api.doctor.DoctorRegistrationData;
 import med.voll.api.patient.DataListingPatient;
 import med.voll.api.patient.Patient;
 import med.voll.api.patient.PatientRegistrationData;
 import med.voll.api.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
